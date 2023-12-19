8871df7191795e3e822af9ca4c9a8d20241f54b8
KlymPikul <klimpikul1989@gmail.com>
2023-12-19 13:06:44 +0100

    add to class CustomList Methods removeByIndex and removeAllByIndex

--- src/CustomList.java
+++ src/CustomList.java
@@ -42,7 +42,7 @@ public class CustomList {
     }
 
     public int get(int index) {
-        return get(index,0);
+        return get(index, 0);
     }
 
     public void add(int element) {
@@ -59,7 +59,140 @@ public class CustomList {
 
     }
 
+    public int getMax() {
+
+        int max = this.list[0];
+        for (int elem : this.list) {
+            if (elem > max) {
+                max = elem;
+            }
+        }
+        return max;
+    }
+
+    public int pop() {
+
+        int lastElement = this.list[this.list.length - 1];
+        int[] newList = Arrays.copyOf(this.list, this.list.length - 1);
+        this.list = newList;
+
+        return lastElement;
+    }
+
+    private void copyArray(int[] init, int[] result) {
+        int[] shorter = (init.length > result.length) ? result : init;
+
+        for (int i = 0; i < shorter.length; i++) {
+            result[i] = init[i];
+        }
+    }
+
+    // Метод remove, удаляющий первое вхождение указанного значения из массива.
+    // Использует метод shiftByIndex для сдвига элементов и pop для удаления последнего элемента.
+    public void remove(int value) {
+        for (int i = 0; i < this.list.length; i++) {
+            if (this.list[i] == value) {
+                shiftByIndex(i);
+                pop();
+                return;
+            }
+        }
+    }
+
+    // Метод remove2, удаляющий первое вхождение указанного значения из массива,
+    // используя метод indexOf для поиска индекса.
+    public void remove2(int value) {
+        int index = indexOf(value);
+        if (index != -1) {
+            shiftByIndex(index);
+            pop();
+        }
+    }
+
+    // Метод removeRight, удаляющий последнее вхождение указанного значения из массива.
+    public void removeRight(int value) {
+        for (int i = this.list.length - 1; i >= 0; i--) {
+            if (this.list[i] == value) {
+                shiftByIndex(i);
+                pop();
+                return;
+            }
+        }
+    }
+
+    //Метод removeAll, удаляющий все вхождения указанного значения из массива.
+    public void removeAll(int value) {
+        for (int i = 0; i < this.list.length; i++) {
+            if (this.list[i] == value) {
+                shiftByIndex(i);
+                pop();
+            }
+        }
+    }
+
+    public void removeAll2(int value) {
+        int i = 0;
+        while (i < this.list.length) {
+            if (this.list[i] == value) {
+                shiftByIndex(i);
+                pop();
+            } else {
+                i++;
+            }
+        }
+    }
+
+    // removeAll2(int value): java public void removeAll2(int value){ int i = 0; while (i < this.list.length){ if (this.list[i] == value){ shiftByIndex(i); pop(); }else{ i++; } } }
+
+    // Метод removeAll2, удаляющий все вхождения указанного значения из массива,
+    //используя цикл while и методы shiftByIndex и pop.
+
+      private void shiftByIndex(int index) {
+       for (int i = index; i < this.list.length - 1; i++) {
+            this.list[i] = this.list[i + 1];
+     }
+     }
+    //Вспомогательный метод shiftByIndex, сдвигающий элементы массива влево начиная с указанного индекса.
 
 
+    // HW. Task1. Реализация метода removeByIndex для удаления элемента по индексу в пользовательском списке (CustomList).
+    public void removeByIndex(int index) {
+        if (index >= 0 && index < this.list.length) {
+            shiftByIndex(index);
+            int[] newList = Arrays.copyOf(this.list, this.list.length - 1);
+            this.list = newList;
+        } else {
+            System.out.println("Некорректный индекс для удаления элемента.");
+        }
+    }
+
+    // HW Task2. Реализация метода removeAllByIndex для удаления всех элементов по индексу в пользовательском списке (CustomList).
+
+    public void removeAllByIndex(int index) {
+        if (index >= 0 && index < this.list.length) {
+            int valueToRemove = this.list[index];
+
+            // Сдвигаем элементы и удаляем все вхождения значения
+            int newSize = 0;
+            for (int i = 0; i < this.list.length; i++) {
+                if (this.list[i] != valueToRemove) {
+                    this.list[newSize] = this.list[i];
+                    newSize++;
+                }
+            }
+
+            // Обновляем размер массива
+            int[] newList = Arrays.copyOf(this.list, newSize);
+            this.list = newList;
+        } else {
+            System.out.println("Некорректный индекс для удаления элемента.");
+        }
+    }
 }
+
+
+
+
+
+
 
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
 import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
+        // int[] numbers = {1, 2, 3, 4, 5};
+        //CustomList customList = new CustomList(numbers);
+
+        CustomList customList = new CustomList(new int[]{4, 3, 6, 6, 13, 6, 5, 2, 16});
+
+
+        System.out.println("массив до удаления: " + customList);
+        customList.removeAllByIndex(2);
+        System.out.println("массив после удаления: " + customList);
+
+
+        int indexToRemove = 3;
+        customList.removeByIndex(indexToRemove);
+        System.out.println("удаляем индекс(" + indexToRemove + "): " + customList);
+
+
+    }
+
+}
+    /*
+
+        //CustomList customList = new CustomList(new int[]{5, 8, 2, 10, 3});
 
         String[] initialStrings = {"Klym", "Maryna", "Nicol"};
         CustomStringList CustomStringList = new CustomStringList(initialStrings);
@@ -22,13 +44,21 @@ public class Main {
 
         CustomStringList.addIfNotExists("Klym");
         CustomStringList.addIfNotExists("Mark");
+
+        System.out.println("Custom List: " + customList);
+        System.out.println("Max Number: " + customList.getMax());
+
+
+        System.out.println(customList.pop());
+        //System.out.println("Popped Element: " + poppedElement);
+        //System.out.println("Updated List: " + customList);
     }
 }
 
 
 
-        /*
-        CustomList customList = new CustomList(new int[]{1, 2, 3, 4, 5, 13, 6, 16});
+
+/*CustomList customList = new CustomList(new int[]{1, 2, 3, 4, 5, 13, 6, 16});
         System.out.println(customList);
         System.out.println(customList.indexOf(3));
         System.out.println(customList.lastIndexOf(4));
@@ -44,7 +74,9 @@ public class Main {
         System.out.println(customList.get(2, -1));
         System.out.println(customList.get(10, -1));
 
-         */
+ */
+
+
 
 
 
